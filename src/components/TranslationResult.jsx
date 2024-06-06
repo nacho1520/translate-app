ad12bb2b7be81cbf9bff04caef2b21ea72064f8e
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-06-06 17:45:51 -0300

    change btn

--- src/components/TranslationResult.jsx
+++ src/components/TranslationResult.jsx
@@ -3,16 +3,23 @@ import AuxiliaryBtn from "./AuxiliaryBtn";
 
 import soundImg from "../assets/sound_max_fill.svg";
 import copyImg from "../assets/Copy.svg";
+import changeImg from "../assets/Horizontal_top_left_main.svg";
 
 const TranslationResult = ({ translation, langs, activeLang, setActiveLang, handleCopy }) => {
     return(
         <div className="w-[560px] p-6 border border-stroke-gray bg-card-dark/80 rounded-3xl">
-            <div className="border-b border-stroke-gray pb-4 pl-2">
+            <div className="w-full flex justify-between items-center border-b border-stroke-gray pb-4 pl-2">
                 <Tabs 
                     options={ langs } 
                     activeLang={ activeLang }
                     setActiveLang={ setActiveLang } 
                 />
+                <AuxiliaryBtn>
+                    <img 
+                        src={ changeImg.src }
+                        style={{ width: "20px", height: "20px" }}
+                    />
+                </AuxiliaryBtn>
             </div>
             <textarea 
                 value={ translation }
