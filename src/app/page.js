586af3949fc388217265b91cb5421bbec8161065
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-06-23 15:39:51 -0300

    update optionsLangs

--- src/app/page.js
+++ src/app/page.js
@@ -8,35 +8,12 @@ import TranslationResult from "@/components/TranslationResult";
 
 
 const optionsLanguage = [
-  {
-    id: 1,
-    lang: "Detect Language",
-  },
-  {
-    id: 2,
-    lang: "English",
-    value: "en",
-  },
-  {
-    id: 3,
-    lang: "French",
-    value: "fr",
-  },
-  {
-    id: 4,
-    lang: "Spanish",
-    value: "es",
-  },
-  {
-    id: 5,
-    lang: "German",
-    value: "de",
-  },
-  {
-    id: 6,
-    lang: "Italian",
-    value: "it",
-  },
+  { id: 1, lang: "Detect Language" },
+  { id: 2, lang: "English", value: "en" },
+  { id: 3, lang: "French", value: "fr" },
+  { id: 4, lang: "Spanish", value: "es" },
+  { id: 5, lang: "German", value: "de" },
+  { id: 6, lang: "Italian", value: "it" },
 ];
 
 const Home = () => {
